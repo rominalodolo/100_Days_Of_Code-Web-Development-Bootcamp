63f80209fab1a911a8d572ff7526c23a0de01980
Romina 로미나 <83961643+rominalodolo@users.noreply.github.com>
2026-03-18 10:44:12 +0200

    Order of importance.

--- operations.js
+++ operations.js
@@ -7,3 +7,8 @@ console.log(10 / 4);
 console.log(10 % 4); // Modulo operator gives the remainder of the division 
 console.log(10 % 3); //10 / 3 gives 3 with a remainder of 1, so the result is 1
 console.log(10 ** 4); // Exponentiation operator gives the result of raising the first operand to the power of the second operand
+
+
+console.log(10 + 4 * 2); // Multiplication has higher precedence than addition, so it is evaluated first. The result is 18.
+console.log((10 + 4) * 2); // Parentheses have the highest precedence, so the addition is evaluated first. The result is 28.
+console.log(10 + 3 - 5.1 *2); // Multiplication is evaluated first, giving 10 + 3 - 10.2, which results in 2.8

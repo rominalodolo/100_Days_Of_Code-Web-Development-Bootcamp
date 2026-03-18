6e5ac42983481bc917902ad0a2c9380fe1d6df7d
Romina 로미나 <83961643+rominalodolo@users.noreply.github.com>
2026-03-18 10:33:03 +0200

    modular operator

--- operations.js
+++ operations.js
@@ -4,4 +4,6 @@ console.log(10 + 4);
 console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
-console.log(10 % 4);
+console.log(10 % 4); // Modulo operator gives the remainder of the division 
+console.log(10 % 3); //10 / 3 gives 3 with a remainder of 1, so the result is 1
+console.log(10 ** 4); // Exponentiation operator gives the result of raising the first operand to the power of the second operand

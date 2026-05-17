b27d113e2a62052a5da3630cb7ce3bc2419c5c97
Romina Lodolo <romina@flume.co.za>
2026-05-17 17:51:50 +0200

    express code instead

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -3,28 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/currenttime', (req, res) => {
-  res.status(200).send('<h1>' + new Date().toISOString() + '</h1>');
+  res.send('<h1>' + new Date().toISOString() + '</h1>');
 });
 
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Home Page!</h1>');
+    });
 
 app.listen(3000)
 
-
-function handleRequest(request, response) {
-  if(request.url === '/currenttime') {
-    response.statusCode = 200;
-    response.end('<h1>' + new Date().toISOString() + '</h1>');
-    return;
-  } else if (request.url ==='/') {
-    response.statusCode = 200;
-    response.end('<h1>Welcome to the Home Page!</h1>');
-    return;
-  }
-}
-
-const server = http.createServer(function);
-
-
-
-server.listen(3000);
 

1168d4c53378d9d753fa12c8cec3fa3e10bce2f0
Romina Lodolo <romina@flume.co.za>
2026-05-17 16:15:09 +0200

    Time stamp

--- NodeJsProject/app.js
+++ NodeJsProject/app.js
@@ -3,17 +3,20 @@ const http = require('http');
 function handleRequest(request, response) {
 
   if(request.url === '/currenttime') {
-    const currentTime = new Date().toLocaleTimeString();
     response.statusCode = 200;
-    response.end(`<h1>Current Time: ${currentTime}</h1>`);
+    response.end('<h1>' + new Date().toISOString() + '</h1>');
+    return;
+  } else if (request.url ==='/') {
+    response.statusCode = 200;
+    response.end('<h1>Welcome to the Home Page!</h1>');
     return;
   }
   // localhost:3000/currenttime;
   // localhost:3000/currentdate;
   // response.statusCode = 404;
 
-  response.statusCode = 200;
-  response.end('<h1>Hello, World!</h1>');
+  // response.statusCode = 200;
+  // response.end('<h1>Hello, World!</h1>');
 }
 
 const server = http.createServer(handleRequest);

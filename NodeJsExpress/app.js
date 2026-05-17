838336ccd9116a72797b9cd1adfedcc90e078100
Romina Lodolo <romina@flume.co.za>
2026-05-17 18:21:03 +0200

    Form code and requests

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: flase }));
+
 app.get('/currenttime', (req, res) => {
   res.send('<h1>' + new Date().toISOString() + '</h1>');
 });
@@ -11,9 +13,13 @@ app.get('/', (req, res) => {
     });
 
 app.get('/form', (req, res) => {
-    res.send('<form><label>Your Name:</label><input type="text" name="name"></form>');
+    res.send('<form action="/submit" method="post"><label>Your Name:</label><input type="text" name="username"> <button type="submit">Submit</button></form>');
 });
 
+app.post('/store-user',function(req, res) => {
+    const userName = req.body.username; // Access the username from the form data
+    console.log('Received username:', userName); // Log the username to the console
+    res.send('<h1>Thank you for submitting your name, ' + userName + '!</h1>'); // Send a response back to the client
+}
+
 app.listen(3000)
-
-

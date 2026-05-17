bfa536d15617d145b84c40a703aaab28a5b32896
Romina Lodolo <romina@flume.co.za>
2026-05-17 19:40:18 +0200

    updating the file

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+require path = require('path');
+
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +22,13 @@ app.get('/form', (req, res) => {
 
 app.post('/store-user',function(req, res) {
     const userName = req.body.username; // Access the username from the form data
+
+    const filePath = path.join(__dirname, 'data', 'users.json'); // Define the path to the file where you want to store the username
+
+    const fileData = fs.readFileSync(filePath); // Read the existing data from the file
+
+    
+    fs.writeFileSync(filePath, JSON.stringify([userName])); // Write the username to the file (you can modify this to append to an array if you want to store multiple usernames)
     console.log('Received username:', userName); // Log the username to the console
     res.send('<h1>Thank you for submitting your name, ' + userName + '!</h1>'); // Send a response back to the client
 });

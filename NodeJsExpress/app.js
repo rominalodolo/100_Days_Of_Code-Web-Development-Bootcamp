eb9e2f50853496ab5cf8fb21198d31b93594a377
Romina Lodolo <romina@flume.co.za>
2026-05-17 18:34:47 +0200

    updated and working

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: flase }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/currenttime', (req, res) => {
   res.send('<h1>' + new Date().toISOString() + '</h1>');

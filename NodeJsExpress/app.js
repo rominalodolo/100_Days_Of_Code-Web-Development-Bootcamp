534344b7c3c210a42bd8390adee1e2026f323cb9
Romina Lodolo <romina@flume.co.za>
2026-05-17 17:56:28 +0200

    form code adding

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Home Page!</h1>');
     });
 
+app.get('/form', (req, res) => {
+    res.send('<form><label>Your Name:</label><input type="text" name="name"></form>');
+});
+
 app.listen(3000)
 
 

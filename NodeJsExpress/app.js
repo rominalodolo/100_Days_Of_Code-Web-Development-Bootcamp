5225c5c8ffd615c09b41b678b78994e01b3dfc72
Romina Lodolo <romina@flume.co.za>
2026-05-17 18:27:09 +0200

    fixes

--- NodeJsExpress/app.js
+++ NodeJsExpress/app.js
@@ -16,10 +16,10 @@ app.get('/form', (req, res) => {
     res.send('<form action="/submit" method="post"><label>Your Name:</label><input type="text" name="username"> <button type="submit">Submit</button></form>');
 });
 
-app.post('/store-user',function(req, res) => {
+app.post('/store-user',function(req, res) {
     const userName = req.body.username; // Access the username from the form data
     console.log('Received username:', userName); // Log the username to the console
     res.send('<h1>Thank you for submitting your name, ' + userName + '!</h1>'); // Send a response back to the client
-}
+});
 
 app.listen(3000)

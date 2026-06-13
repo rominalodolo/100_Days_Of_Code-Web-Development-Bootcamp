ca602d5a87135b64ec6b48affd83fddf19a0ddf5
Romina Lodolo <romina@flume.co.za>
2026-06-14 00:29:42 +0200

    connection / promise

--- Blog-Project/data/database.js
+++ Blog-Project/data/database.js
@@ -13,4 +13,11 @@ module.exports = pool;
 // const mongodb = require('mongodb');
 // const MongoClient = mongodb.MongoClient;
 
+// async function connectToDatabase() {
+//   const client = await MongoClient.connect('mongodb://localhost:27017');
+//   return client.db('blog');
+// }
+
+// module.exports = connectToDatabase;
+
 //MongoClient.connect('mongodb://localhost:27017');
